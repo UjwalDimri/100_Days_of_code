336b8a51f6c77314985c9517eab1ee7e31d280c7
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-26 22:20:14 +0530

    Twelthcode

--- Day6_2.c
+++ Day6_2.c
@@ -18,5 +18,23 @@
 // Positive 
 #include<stdio.h>
 int main(){
+    int n;
+    printf("Enter any Number :");
+    scanf("%d",&n);
+    if(n>=0){
+        if (n==0){
+        printf("Zero");
+        }
+        else {
+        printf("positive");
+        }
+    }    
     
+
+    else if (n<0){
+        printf("negative");
+    }
+    return 0;
 }
+
+ 
