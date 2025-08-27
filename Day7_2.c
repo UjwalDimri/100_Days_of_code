20617d83ba698eeb0550634fa6720908a48a8d8a
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-27 16:04:42 +0530

    Thirteenth Code

---
+++ Day7_2.c
@@ -0,0 +1,18 @@
+// 🔀
+// Q14 (Conditional Statements)
+// 📋
+// Write a program to input a character and check whether it is a vowel or consonant using if–else.
+
+// Show Sample Test Cases
+// Input 1:
+// a
+// Output 1:
+// Vowel
+// Input 2:
+// b
+// Output 2:
+// Consonant
+#include<stdio.h>
+int main(){
+    
+}
