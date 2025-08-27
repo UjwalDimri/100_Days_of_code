4af05c191d69827833185c257b8fc8913b88d552
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-27 16:49:27 +0530

    Fourteenth Code

--- Day7_2.c
+++ Day7_2.c
@@ -14,5 +14,13 @@
 // Consonant
 #include<stdio.h>
 int main(){
-    
+    char ch ;
+    printf("Enter a Character");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
+        printf("Vowel");
+    }else{
+        printf("consonant");
+    }
+    return 0;
 }
