026ed6477bb191f8132428d874f0d2beebb47901
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-29 00:12:41 +0530

    Fifteenth Code

---
+++ Day8_2.c
@@ -0,0 +1,14 @@
+// 🔀
+// Q16 (Conditional Statements)
+// 📋
+// Write a program to input three numbers and find the largest among them using if–else.
+
+// Show Sample Test Cases
+// Input 1:
+// 3 7 5
+// Output 1:
+// Largest is 7
+// Input 2:
+// -1 -5 0
+// Output 2:
+// Largest is 0
