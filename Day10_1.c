664487eeae2ff11943843715ead0ae3fafb61d08
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-30 10:17:34 +0530

    Twentieth code

--- Day10_1.c
+++ Day10_1.c
@@ -21,9 +21,9 @@ int main(){
     int x,y,z;
     printf("Enter three sides of triangle :");
     scanf("%d %d %d",&x,&y,&z);
-    if((x=y)&&(y=z)&&(z=x)){
+    if((x==y)&&(y==z)&&(z==x)){
         printf("Equilateral");
-    }else if((x=y)&&(y=z)||(y=z)&&(z=x)){
+    }else if((x==y)||(y==z)||(z==x)){
         printf("Isosecles");
     }else if((x!=y)&&(y!=z)&&(z!=x)){
         printf("Scalene");
