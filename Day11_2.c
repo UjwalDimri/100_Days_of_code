e1512bf88f062549ac58693c24c51b82ca35f349
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-31 14:00:51 +0530

    Twentyfirst Code

---
+++ Day11_2.c
@@ -0,0 +1,18 @@
+// 🔀
+// Q22 (Conditional Statements)
+// 📋
+// Write a program to find profit or loss percentage given cost price and selling price.
+
+// Show Sample Test Cases
+// Input 1:
+// 1000 1200
+// Output 1:
+// Profit 20%
+// Input 2:
+// 1000 800
+// Output 2:
+// Loss 20%
+// Input 3:
+// 1000 1000
+// Output 3:
+// No Profit No Loss
