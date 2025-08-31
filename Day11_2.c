3989983f7c7e9c29331606da6197d90a928f3d4a
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-31 14:25:04 +0530

    TwentySecond code

--- Day11_2.c
+++ Day11_2.c
@@ -15,4 +15,22 @@
 // Input 3:
 // 1000 1000
 // Output 3:
-// No Profit No Loss
+// No Profit No Loss 
+#include<stdio.h>
+int main() {
+    float a ,sp ,cp ,p,loss ;
+    printf("Enter cost price and selling prince :");
+    scanf("%f %f",&cp,&sp);
+    a=sp-cp;
+    if(a>0){
+        p= (a/cp)*100;
+        printf("Profit %f %%",p);
+
+    } else if (a<0){
+        loss= -(a/cp)*100;
+        printf("Loss %f %%",loss);
+    }else if(a==0){
+        printf("No Profit No Loss");
+    }
+    return 0;
+}
