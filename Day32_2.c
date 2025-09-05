6bebb8c058c1226732b68e3c1ab7579aeaebe1e1
Ujwal dimri <ujwaldimri223@gmail.com>
2025-09-05 23:00:57 +0530

    ThirtyFirst Code

---
+++ Day32_2.c
@@ -0,0 +1,17 @@
+// 🔢
+// Q64 (Arrays (1D))
+// 📋
+// Find the digit that occurs the most times in an integer number.
+// Show Sample Test Cases
+// Input 1:
+// 112233
+// Output 1:
+// 1
+// Input 2:
+// 887799
+// Output 2:
+// 7
+#include<stdio.h>
+int main(){
+    int 
+}
