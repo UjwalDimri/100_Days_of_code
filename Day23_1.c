76778461a24a35e96c4b0cad71bd326d3c104282
Ujwal Dimri <ujwaldimri223@gmail.com>
2025-09-19 19:59:22 +0530

    Forty Five

---
+++ Day23_1.c
@@ -0,0 +1,33 @@
+// 🔁
+// Q45 (Loops without Arrays/Strings)
+// 📋
+// Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
+// Show Sample Test Cases
+// Input 1:
+// 3
+// Output 1:
+// Approximate sum: 1.56
+// Input 2:
+// 5
+// Output 2:
+// Approximate sum: 2.22
+#include<stdio.h> 
+
+int main()
+{
+    int n , i ; 
+    double sum = 0 ;
+
+    printf("Enter the value of n : "); 
+    scanf("%d",&n); 
+
+    for(i=1 ; i<=n ; i++)
+    {
+        sum = sum + (float)(2*n)/(4*n-1); // n th term of series is 2n/(4n-1)
+    }
+
+    printf("Sum of first %d terms of the following series is %f",n,sum); 
+
+    return 0 ; 
+}
