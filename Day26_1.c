1e9dc12665897ecf759bf60eff865eedc818913a
Ujwal dimri <ujwaldimri223@gmail.com>
2025-10-23 14:44:43 +0530

    FifttyFirst Code

---
+++ Day26_1.c
@@ -0,0 +1,23 @@
+// 🔄
+// Q51 (Nested Loops without Arrays/Strings)
+// 📋
+// Write a program to print the following pattern:
+//     5
+//    45
+//   345
+//  2345
+// 12345
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+            printf(" ");
+        }
+        for(int k=5-i+1;k<=5;k++){
+            printf("%d",k);
+        }
+       printf("\n");
+    }
+ 
+    return 0;
+}
