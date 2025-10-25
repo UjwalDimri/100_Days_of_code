978fd8278b0a485a600e51aa700538e911f4a825
Ujwal dimri <ujwaldimri223@gmail.com>
2025-10-25 15:34:42 +0530

    FiftySecond Code

---
+++ Day26_2.c
@@ -0,0 +1,27 @@
+// 🔄
+// Q52 (Nested Loops without Arrays/Strings)
+// 📋
+// Write a program to print the following pattern:
+
+// *
+
+// *
+// *
+// *
+
+// *
+// *
+// *
+// *
+// *
+
+// *
+// *
+// *
+
+// *
+
+// Show Sample Test Cases
+// Input 1:
+// Output 1:
+// Pattern with stars spaced irregularly as shown. 
