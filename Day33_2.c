db52e7823a80911220c48b05c27e0fd4d75cdf41
Ujwal dimri <ujwaldimri223@gmail.com>
2025-10-25 16:56:31 +0530

    SixtySixth Code

---
+++ Day33_2.c
@@ -0,0 +1,40 @@
+// 🔢
+// Q66 (Arrays (1D))
+// 📋
+// Insert an element in a sorted array at the appropriate position.
+// Show Sample Test Cases
+// Input 1:
+// 5
+// 1 2 4 5 6
+// 3
+// Output 1:
+// 1 2 3 4 5 6
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of element do you want to enter :");
+    scanf("%d",&n);
+    int arr[n+1];
+    for(int i=0;i<n;i++){
+       printf("Enter a Number :");
+       scanf("%d",&arr[i]);
+    }
+    int a,x=n;
+    printf("Enter the number which you want to insert:");
+    scanf("%d",&a);
+    for(int i=0;i<=n;i++){
+        if(arr[i]>a){
+            x=i;
+            break;
+        }
+    }
+    
+    for(int i=n;i>x;i--){
+            arr[i]=arr[i-1];
+        }
+    arr[x]=a;
+     for(int i=0;i<=n;i++){
+       printf("%d \t",arr[i]);
+    }    
+    }
+    
