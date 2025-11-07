e2ce79e1baab7704dfcf179a22b69da2b973dc26
Ujwal dimri <ujwaldimri223@gmail.com>
2025-11-08 01:28:13 +0530

    SeventySixth Code

---
+++ Day38_2.c
@@ -0,0 +1,21 @@
+// 🔲
+// Q76 (2D Arrays)
+// 📋
+// Check if a matrix is symmetric.
+// Show Sample Test Cases
+// Input 1:
+// 2 2
+// 1 2
+// 2 1
+// Output 1:
+// True
+// Input 2:
+// 2 2
+// 1 0
+// 2 1
+// Output 2:
+// False 
+#include<stdio.h>
+int main(){
+    
+}
