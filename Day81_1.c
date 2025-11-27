4ade36870ecbde03399c309379da8b5e2842a8ba
Ujwal dimri <ujwaldimri223@gmail.com>
2025-11-27 10:45:51 +0530

    One Hundred Thirty First Code

---
+++ Day81_1.c
@@ -0,0 +1,17 @@
+// 🧱
+// Q131 (Enum)
+// 📋
+// Create an enumeration for days (SUNDAY to SATURDAY) and print each day with its integer value.
+// Show Sample Test Cases
+// Input 1:
+// No input
+// Output 1:
+// SUNDAY = 0
+// MONDAY = 1
+// TUESDAY = 2
+// WEDNESDAY = 3
+// THURSDAY = 4
+// FRIDAY = 5
+// SATURDAY = 6
+// Explanation 1:
+// Each day in the enum is automatically assigned an integer starting from 0.
